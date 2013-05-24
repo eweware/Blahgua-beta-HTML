7dba368ba6e1a90ef40652e1c2dbf9f57ccb6bfe
davevr <davevr@eweware.com>
2013-05-23 18:10:57 -0700

    start on refactored global modules

---
+++ scripts/constants.js
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: davev_000
+ * Date: 5/23/13
+ * Time: 6:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+define('constants',
+    [],
+    function () {
+
+        var Initialize = function() {
+            // placeholder
+        };
+
+        return {
+            Initialize :   Initialize
+        }
+    }
+);
---
+++ scripts/globals.js
@@ -0,0 +1,29 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: davev_000
+ * Date: 5/23/13
+ * Time: 6:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: davev_000
+ * Date: 5/23/13
+ * Time: 6:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+define('globals',
+    [],
+    function () {
+
+        var Initialize = function() {
+            // placeholder
+        };
+
+        return {
+            Initialize :   Initialize
+        }
+    }
+);
