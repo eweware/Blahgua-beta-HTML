8db505090079b0e5c5cba682f53bab036848b981
Dave Vronay <davevr@advanced-ux.com>
2013-06-24 11:56:03 -0700

    CSS fixes

--- scripts/blahgua_base.js
+++ scripts/blahgua_base.js
@@ -422,7 +422,7 @@ define('blahgua_base',
         banner.appendChild(label);
         banner.channelLabel = label;
         var caret = document.createElement("i");
-        caret.className = "channel-dropdown icon-chevron-down";
+        caret.className = "channel-dropdown icon-chevron-sign-down";
         banner.appendChild(caret);
 
 
