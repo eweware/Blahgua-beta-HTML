4e4caac6d8df74a7be67504d14c5e61f9572e49b
Yang <Yang@163.com>
2013-06-26 10:29:32 +0800

    fix bug(BWC1252)Profile Page: User Accounts -> Username should be displayed as text

--- scripts/pagescripts/SelfPageDetails.js
+++ scripts/pagescripts/SelfPageDetails.js
@@ -39,7 +39,8 @@ define('SelfPageDetails',
 
 
         var RefreshPage = function() {
-            $("#userName").val(G.CurrentUser.N);
+		    $("#userName").text(G.CurrentUser.N);
+           // $("#userName").val(G.CurrentUser.N);
             var nickName = G.GetSafeProperty(G.UserProfile, "A", "someone");
             $("#NicknameInput").val(nickName).attr("initial-value", nickName);
             //image
