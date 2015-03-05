357383b78497e4276e32a5e43e83f55e7b251495
Dave Vronay <davevr@eweware.com>
2015-03-05 15:05:51 -0800

    group import tool work

--- scripts/pagescripts/ViewGroupPage.js
+++ scripts/pagescripts/ViewGroupPage.js
@@ -7,6 +7,7 @@ define('ViewGroupPage',
     function (G, exports, blahgua_rest) {
 
         var newChannel = null;
+        var perms = null;
 
         var InitializePage = function(theGroup) {
             this.newChannel = theGroup;
@@ -19,12 +20,59 @@ define('ViewGroupPage',
 
             $(".fullBlahgerName").text(theGroup.N);
 
+            blahgua_rest.GetChannelPermissionById(theGroup._id, function(thePerms)
+            {
+                perms = thePerms;
+                if (perms.admin) {
+                    $("#AdminPanel").show();
+                }
+                else
+                    $("#AdminPanel").hide();
+            })
 
+            $("#LoadRSSBtn").click(HandleRSSLoad);
+
+            $("#ImportDataBtn").click(HandleRSSImport);
 
         };
 
 
+        var HandleRSSLoad = function (theEvent) {
+            var theURL = $("#RSSURLfield").val();
+            $.ajax({
+                url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(theURL),
+                dataType: 'json',
+                success: function(data) {
+                    //console.log(data.responseData.feed);
+                    $("#RSSBody").html('<tr><td colspan="2">'+ data.responseData.feed.title +'</td></tr>');
 
+                    $.each(data.responseData.feed.entries, function(key, value){
+                        var thehtml = '<tr><td colspan="2"><a href="'+value.link+'" target="_blank">'+value.title+'</a></td></tr>';
+                        $("#RSSBody").append(thehtml);
+                    });
+                    var theItems = $("#RSSBody a");
+
+                    $.each(theItems, function (theKey, theVal) {
+                        var curHRef = theVal.href;
+                        if (curHRef != "") {
+                            $.ajax({
+                                url: 'http://api.embed.ly/1/extract?key=400ca94d281f4b77b94b351c345d6ba8&maxwidth=500&url=' + encodeURIComponent(curHRef),
+                                dataType: 'json',
+                                success: function(theObject) {
+                                    $(theVal).title="done";
+                                }
+
+                            });
+                        }
+                    });
+                }
+            });
+        };
+
+        var HandleRSSImport = function (theEvent) {
+
+
+        };
 
 
         return {
